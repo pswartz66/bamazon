7c0a5dda888cc3762ed9b7b5a499949039e09b46
Phil Swartz <pswartz.sju@gmail.com>
2019-10-23 22:52:56 -0400

    commented out line in sql and ran a basic query to add rows into the products table

--- bamazon.sql
+++ bamazon.sql
@@ -4,16 +4,17 @@ CREATE DATABASE bamazonDB;
 
 USE bamazonDB;
 
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY mysql_native_password;
+-- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY mysql_native_password;
 
 
-CREATE TABLE products (
+CREATE TABLE products(
 
-    item_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(30) NULL,
     dept_name VARCHAR(30) NULL,
     price DECIMAL(10, 2) NULL,
-    stock_quantity INT default 0
+    stock_quantity INT default 0,
+    PRIMARY KEY (id)
 
 );
 
