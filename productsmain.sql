b45570bb671165ae9425b334599987b14ef6e5dd
Phil Swartz <pswartz.sju@gmail.com>
2019-10-26 10:26:51 -0400

    saved the productsMain sql file

---
+++ productsmain.sql
@@ -0,0 +1,11 @@
+-- SELECT * FROM bamazonDB.productsmain WHERE shelf_price BETWEEN 1 and 10 ORDER BY rand() LIMIT 10;
+
+
+-- SELECT * FROM bamazonDB.productsmain WHERE item_description LIKE '%JIM BEAM%';
+
+-- AS 'Currency Format' FROM bamazonDB.productsmain;
+-- SELECT * FROM bamazonDB.productsmain ORDER BY item_no LIMIT 100;
+
+-- SELECT * FROM bamazonDB.productsmain LIMIT 10;
+
+-- SELECT * FROM bamazonDB.productsmain WHERE shelf_price > 2000;
